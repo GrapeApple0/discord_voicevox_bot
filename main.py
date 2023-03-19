cd2c71ef4b7fe120a20af52b4e1061cab29abfd4
GrapeApple <budouapplenoki@gmail.com>
2023-03-19 09:49:21 +0900

    スラッシュコマンドへの対応/キャラクターをjsonファイル化

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import discord
+from discord import app_commands
 import requests
 import json
 import uuid
-import time
 import os
 from dataclasses import dataclass
 import re
+from typing import Literal
 from dotenv import load_dotenv
-#load_dotenv()
-
+load_dotenv()
 VV_HOST = os.getenv("VV_HOST")
-
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-speaker_list = [3, 2, 8, 10, 9, 11, 12, 13, 14, 16, 20, 21, 23, 27, 29, 42, 43, 46, 47]
-speakername_list = ["ずんだもん","四国めたん","春日部つむぎ","雨晴はう","波音リツ","玄野武宏","白上虎太郎","青山龍星","冥鳴ひまり","九州そら", "もち子さん","剣崎雌雄","WhiteCUL", "後鬼","No.7","ちび式じい","櫻歌ミコ","小夜/SAYO","ナースロボ＿タイプＴ"]
-
-@dataclass #これまだ使っていない、後でこれを使うように良い感じにする
+@dataclass
 class Speaker:
     id: int
     name: str
 
+with open("speakers.json","r") as f:
+    #object_hook 第一引数に辞書型で帰ってくるのをいじって返す
+    #speaker_dict = json.load(f)#,object_hook=lambda t:Speaker(t["id"],t["name"]))
+    speakers = [Speaker(speaker["id"],speaker["name"]) for speaker in json.load(f)]
 
 @dataclass
 class ConectedUser:
     user_id: int
     voicevox_id: int
-
 
 class ConnectedChannel:
     text_channel_id: int
@@ -41,9 +40,9 @@ class ConnectedChannel:
         self.select_speaker_index = 0
     def say(self, content: str, user_id: int, message: discord.Message):
         if len(list(filter(lambda user : user.user_id == user_id, self.users))) == 0:
-            self.users.append(ConectedUser(user_id, speaker_list[self.select_speaker_index]))
+            self.users.append(ConectedUser(user_id, speakers[self.select_speaker_index].id))
             self.select_speaker_index += 1
-            if len(speaker_list) == self.select_speaker_index:
+            if len(speakers) == self.select_speaker_index:
                 self.select_speaker_index = 0
         target = list(filter(lambda user : user.user_id == user_id, self.users))[0]
         filename = str(uuid.uuid4())+".wav"
@@ -76,9 +75,10 @@ def generate_wav(text, speaker=1, filename='audio.wav'):
     with open("./audio/"+filename, 'wb') as f:
         f.write(response2.content)
 
-def get_speaker_info(spaker_id: int) -> str:
-    index = speaker_list.index(spaker_id)
-    return speakername_list[index]
+def get_speaker_info(speaker_name: str = None,speaker_id: int = None) -> Speaker:
+    if speaker_id != None:
+        return list(filter(lambda x: x.id == speaker_id, speakers))[0]
+    return list(filter(lambda x: x.name == speaker_name, speakers))[0]
 
 def find_url(content: str):
     return re.findall('https?://[A-Za-z0-9_/:%#$&?()~.=+-]+?(?=https?:|[^A-Za-z0-9_/:%#$&?()~.=+-]|$)', content)
@@ -89,101 +89,99 @@ def find_mention(content: str):
 
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(1032914541195055149))
     print(f'We have logged in as {client.user}')
+
+guild_ids = [discord.Object(1032914541195055149)] # Put your server ID in this array.
+
+@tree.command(name="connect",description="実行したユーザーが接続中のボイスチャンネルに接続します。",guilds=guild_ids)
+async def connect_vc(interaction: discord.Interaction):
+    if interaction.user.voice is None:
+        await interaction.response.send_message("実行者がボイスチャンネルに入っていないため接続できませんでした。")
+        return
+    await interaction.user.voice.channel.connect()
+    await interaction.response.send_message("接続しました。")
+    connected_channels.append(ConnectedChannel(interaction.channel.id))
+    print(connected_channels)
+
+@tree.command(name="disconnect",description="ボイスチャンネルから切断します。",guilds=guild_ids)
+async def disconnect_vc(interaction: discord.Interaction):
+    if interaction.user.voice is None:
+        await interaction.response.send_message("botがボイスチャンネルに接続されていないため切断できませんでした。")
+        return
+
+    await interaction.guild.voice_client.disconnect()
+    connected_channels.remove(list(filter(lambda channel : channel.text_channel_id == interaction.channel.id ,connected_channels))[0])
+    print(connected_channels)
+    await interaction.response.send_message("切断しました。")
+
+@tree.command(name="speakerinfo",description="各ユーザーごとに割り当てられているキャラクター一覧を表示します。",guild=discord.Object(1032914541195055149))
+async def speakerinfo(interaction: discord.Interaction):
+    await interaction.response.defer()
+    channels = list(filter(lambda channel : channel.text_channel_id == interaction.channel.id, connected_channels))
+    if len(channels) > 0:
+        users = list(filter(lambda user : user.user_id == interaction.user.id ,channels[0].users))
+        if len(users) > 0:
+            speaker_name = get_speaker_info(speaker_id = users[0].voicevox_id).name
+            await interaction.followup.send(interaction.user.display_name + ":" + speaker_name)
+            return
+    await interaction.followup.send("設定されているユーザーはいません。")
+
+@tree.command(name="setspeaker",description="使用するキャラクターを設定します。",guilds=guild_ids)
+async def set_speaker(interaction: discord.Interaction, name: Literal[tuple([str(speaker.name) for speaker in speakers])]):
+    await interaction.response.defer()
+    if name not in [str(speaker.name) for speaker in speakers]:
+        await interaction.followup.send("正しくない形式です")
+        return
+    channels = list(filter(lambda channel : channel.text_channel_id == interaction.channel.id, connected_channels))
+    if len(channels) > 0:
+        users = list(filter(lambda user : user.user_id == interaction.user.id ,channels[0].users))
+        if len(users) < 0:
+            await interaction.followup.send("喋るユーザーとして登録されていません。なにか喋ってからもう一度試してください。")
+        elif len(users) > 0:
+            users[0].voicevox_id = get_speaker_info(name).id
+            await interaction.followup.send(f"{interaction.user.display_name}さんの声を「{name}」にしました")
+        return
+    await interaction.followup.send("ボイスチャンネルに入っていないためコマンドを実行できませんでした。")
+
+@tree.command(name="speakerlist",description="使用できるキャラクター一覧を表示します。",guilds=guild_ids)
+async def speaker_list(interaction: discord.Interaction):
+    await interaction.response.send_message("\n".join([str(speaker.name) for speaker in speakers]))
+
+@tree.command(name="listdic",description="追加済みのユーザー辞書の単語一覧を表示します。",guilds=guild_ids)
+async def speaker_list(interaction: discord.Interaction):
+    res = requests.get(f'http://{host}:{port}/user_dict')
+    dic_json = res.json()
+    await interaction.response.send_message("ID:表記:発音(カタカナ):音が下がる場所\n" + "\n".join([f"{key}:{dic_json[key]['surface']}:{dic_json[key]['pronunciation']}:{dic_json[key]['accent_type']}" for key in dic_json]))
+
+@tree.command(name="adddic",description="ユーザー辞書に単語を追加します。",guilds=guild_ids)
+async def add_dic(interaction: discord.Interaction,surface:str,pronunciation:str,accent:int):
+    params = (
+        ('surface', surface),
+        ('pronunciation', pronunciation),
+        ('accent_type', accent)
+    )
+    res = requests.post(f'http://{host}:{port}/user_dict_word', params=params)
+    if res.ok:
+        await interaction.response.send_message(":white_check_mark:追加に成功しました")
+    else:
+        await interaction.response.send_message(":dizzy_face:追加に失敗しました")
+        print(res.text)
+
+@tree.command(name="deldic",description="追加済みのユーザー辞書の単語一覧を表示します。",guilds=guild_ids)
+async def del_dic(interaction: discord.Interaction,id:str):
+    res = requests.delete(f'http://{host}:{port}/user_dict_word/{id}')
+    if res.ok:
+        await interaction.response.send_message(":wastebasket:削除に成功しました")
+    else:
+        await interaction.response.send_message(":dizzy_face:削除に失敗しました")
+        print(res.text)
 
 @client.event
 async def on_message(message: discord.Message):
     # メッセージの送信者がbotだった場合は無視する
     if message.author.bot:
         return
-
-    if message.content == "!join":
-        if message.author.voice is None:
-            await message.channel.send("あなたはボイスチャンネルに接続していません。")
-            return
-        # ボイスチャンネルに接続する
-        await message.author.voice.channel.connect()
-        #message.guild.voice_client.play(discord.FFmpegPCMAudio("example.mp3"))
-        await message.channel.send("接続しました。")
-        connected_channels.append(ConnectedChannel(message.channel.id))
-        print(connected_channels)
-
-    elif message.content == "!leave":
-        if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
-            return
-
-        # 切断する
-        await message.guild.voice_client.disconnect()
-        connected_channels.remove(list(filter(lambda channel : channel.text_channel_id == message.channel.id ,connected_channels))[0])
-        print(connected_channels)
-
-        await message.channel.send("切断しました。")
-    elif message.content == "!sayhello":
-        if message.guild.voice_client is None:
-            await message.channel.send("接続していません。")
-            return
-    elif message.content == "!speakerinfo":
-        channels = list(filter(lambda channel : channel.text_channel_id == message.channel.id ,connected_channels))
-        if len(channels) > 0:
-            users = list(filter(lambda user : user.user_id == message.author.id ,channels[0].users))
-            if len(users) > 0:
-                speaker_name = get_speaker_info(users[0].voicevox_id)
-                await message.channel.send(message.author.display_name + ":" + speaker_name)
-    elif message.content.find("!setspeaker") != -1:
-        channels = list(filter(lambda channel : channel.text_channel_id == message.channel.id ,connected_channels))
-        if len(channels) > 0:
-            users = list(filter(lambda user : user.user_id == message.author.id ,channels[0].users))
-            if len(users) > 0:
-                try:
-                    name = message.content.split(">")[1]
-                    index = speakername_list.index(name)
-                    users[0].voicevox_id = speaker_list[index]
-                    await message.channel.send(f"{message.author.display_name}さんの声を「{name}」にしました")
-                except:
-                    await message.channel.send("正しくない形式です")
-            else:
-                await message.channel.send("喋るユーザーに登録されていません。何か書き込んでから再度試して下さい")
-    elif message.content == "!listspeaker":
-        rtnstr = ""
-        for name in speakername_list:
-            rtnstr += name + "\n"
-        await message.channel.send(rtnstr)
-    elif message.content == "!listdict":
-        res = requests.get(f'http://{host}:{port}/user_dict')
-        rtnstr = ""
-        jsonres = res.json()
-        rtnstr += f"ID:表記:発音（カタカナ）:音が下がる場所\n"
-        for key in jsonres:
-            rtnstr += f"{key}:{jsonres[key]['surface']}:{jsonres[key]['pronunciation']}:{jsonres[key]['accent_type']}\n"
-        await message.channel.send(rtnstr)
-    elif message.content.find("!adddict") != -1:
-        cmds = message.content.split(">")
-        if len(cmds) != 4 or int(cmds[3]) > len(cmds[2]):
-            await message.channel.send("引数が不正です")
-            return
-        params = (
-            ('surface', cmds[1]),
-            ('pronunciation', cmds[2]),
-            ('accent_type', int(cmds[3]))
-        )
-        res = requests.post(f'http://{host}:{port}/user_dict_word', params=params)
-        if res.ok:
-            await message.channel.send(":white_check_mark:追加に成功しました")
-        else:
-            await message.channel.send(":dizzy_face:追加に失敗しました")
-            print(res.text)
-    elif message.content.find("!deldict") != -1:
-        cmds = message.content.split(">")
-        if len(cmds) != 2:
-            await message.channel.send("引数が不正です")
-            return
-        res = requests.delete(f'http://{host}:{port}/user_dict_word/{cmds[1]}')
-        if res.ok:
-            await message.channel.send(":wastebasket:削除に成功しました")
-        else:
-            await message.channel.send(":dizzy_face:削除に失敗しました")
-            print(res.text)
     else:
         channels = list(filter(lambda channel : channel.text_channel_id == message.channel.id ,connected_channels))
         if len(channels) > 0:
@@ -194,7 +192,7 @@ async def on_message(message: discord.Message):
             print(content)
             print(urls)
             for url in urls:
-                content = content.replace(url,"")
+                content = content.replace(url,"URL")
             for stamp in stamps:
                 content = content.replace(stamp,"")
             for mention in mentions:
@@ -203,7 +201,6 @@ async def on_message(message: discord.Message):
                 return
             channels[0].say(content, message.author.id, message)
 
-
-
 # Botのトークンを指定（デベロッパーサイトで確認可能）
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
+
